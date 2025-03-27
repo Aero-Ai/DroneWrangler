b1254efd139b0dace7c5db8ede22766ca0eb01c1
Brent <bshafer93@gmail.com>
2025-03-27 08:19:27 -0500

    Updated link expiration

--- functions/zip-and-send/index.js
+++ functions/zip-and-send/index.js
@@ -11,67 +11,16 @@ exports.handler = async (event) => {
         // Configuration - replace with your values
         const sourceBucket = 'user-drone-data-dev';
         const targetBucket = 'user-drone-data-dev/brentshaferImagePipelineFullTest01';
-        const folderPaths = ['brentshaferImagePipelineFullTest01/output/']; // Folders to zip
-        const zipFileName = `output-${Date.now()}.zip`;
-        const recipientEmail = 'bshafer93@gmail.com'; // This could be passed in the event
+        const zipFileName = `odmoutput.zip`;
+        const recipientEmail = 'bshafer93@gmail.com'; 
         
-        // Create a pass-through stream for the zip file
-        const passthrough = new stream.PassThrough();
-        
-        // Create zip archive
-        const archive = archiver('zip', {
-            zlib: { level: 9 } // Maximum compression
-        });
-        
-        // Set up the S3 upload parameters
-        const uploadParams = {
-            Bucket: targetBucket,
-            Key: zipFileName,
-            Body: passthrough,
-            ContentType: 'application/zip'
-        };
-        
-        // Start the S3 upload
-        const uploadPromise = s3.upload(uploadParams).promise();
-        
-        // Pipe archive data to the passthrough stream
-        archive.pipe(passthrough);
-        
-        // Process each folder
-        for (const folderPath of folderPaths) {
-            // List all objects in the folder
-            const listParams = {
-                Bucket: sourceBucket,
-                Prefix: folderPath
-            };
-            
-            const listedObjects = await s3.listObjectsV2(listParams).promise();
-            
-            // Add each file to the archive
-            for (const object of listedObjects.Contents) {
-                if (object.Key !== folderPath) { // Skip folder itself
-                    const fileData = await s3.getObject({
-                        Bucket: sourceBucket,
-                        Key: object.Key
-                    }).promise();
-                    
-                    archive.append(fileData.Body, { name: object.Key });
-                }
-            }
-        }
-        
-        // Finalize the archive
-        await archive.finalize();
-        
-        // Wait for the upload to complete
-        await uploadPromise;
 
-        // Generate a pre-signed URL that expires in 7 days (604800 seconds)
+        // Generate a pre-signed URL that expires in 2 days (172800 seconds)
         const downloadUrl = s3.getSignedUrl('getObject', {
           Bucket: targetBucket,
           Key: zipFileName,
           ResponseContentDisposition: 'attachment;',
-          Expires: 604800
+          Expires: 172800
       });
       
       // Send email with the download link
@@ -86,7 +35,7 @@ exports.handler = async (event) => {
               },
               Body: {
                   Text: {
-                      Data: `Hello,\n\nYour requested files have been packaged. You can download them using this link:\n${downloadUrl}\n\nThis link will expire in 7 days.\n\nThank you!`
+                      Data: `Hello,\n\nYour requested files have been packaged. You can download them using this link:\n${downloadUrl}\n\nThis link will expire in 2 days.\n\nThank you!`
                   }
               }
           }
@@ -97,7 +46,7 @@ exports.handler = async (event) => {
 
         return {
             statusCode: 200,
-            body: 'Files successfully zipped and email sent'
+            body: 'File link email sent!'
         };
     } catch (error) {
         console.error('Error:', error);
